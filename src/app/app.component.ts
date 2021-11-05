a46053d708688b499cbcea527e9f4e84a2c3e4a2
Drile99 <nikoladrikic5@gmail.com>
2021-11-05 11:01:46 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-simple-new-app';
+  
+  num1:number=0;
+  num2:number=0;
+  num3:number=0;
+  fakt:number=0;
+  num4:number=0;
+  
+  
+  add(){
+    this.num3 = this.num1 + this.num2;
+  }
+  
+  sub(){
+    this.num3= this.num1 - this.num2;
+  }
+  
+  multiply()
+  {
+    this.num3= this.num1 * this.num2;
+  }
+  
+  divide()
+  {
+    this.num3= this.num1 / this.num2;
+  }
+  
+  faktorijel()
+  {
+
+    
+    if(this.fakt===0)
+    {
+      this.num4=1;
+    }
+    else {
+      for(let i=1;i<=this.fakt;i++)
+      {
+        this.num4 = this.num4 * i; 
+      }
+
+      this.fakt=0;
+        }
+    
+  }
+  
 }
